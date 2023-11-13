5ea1e5f4931a6d389c8bea70dddda4e7575a4e6d
qiyu6 <qiyu6@illinois.edu>
2023-11-13 01:11:52 -0600

    feat: complete About Tech-Stack sub-section

--- index.js
+++ index.js
@@ -18,3 +18,57 @@ function handleNavItemClick() {
   togglerLines[1].classList.remove("toggler-cross-2");
   togglerLines[2].classList.remove("toggler-cross-3");
 }
+
+// Part3: About
+tech_proficiency_map = {
+  javascript: "95%",
+  python: "85%",
+  java: "75%",
+  cpp: "65%",
+  go: "60%",
+  html: "90%",
+  css: "90%",
+  react: "90%",
+  rn: "70%",
+  typescript: "75%",
+  redux: "60%",
+  vue: "60%",
+  nodejs: "80%",
+  express: "70%",
+  mysql: "85%",
+  dynamodb: "70%",
+  mongodb: "60%",
+  git: "85%",
+  pytorch: "90%",
+  cuda: "75%",
+  opencv: "70%",
+};
+
+document.querySelectorAll(".about-tech-cat-list-item").forEach((techItem) =>
+  techItem.addEventListener("mouseenter", (e) => {
+    const item = e.target;
+    document
+      .querySelector(".about-item-tech-group-list")
+      .style.setProperty(
+        "--tech-proficency-bar-width",
+        tech_proficiency_map[item.id]
+      );
+    item
+      .querySelector(".tech-item-detail")
+      .classList.add("tech-item-detail-show");
+    item.querySelector(".proficiency-bar").classList.add("progressing");
+  })
+);
+
+document.querySelectorAll(".about-tech-cat-list-item").forEach((techItem) =>
+  techItem.addEventListener("mouseleave", (e) => {
+    const item = e.target;
+    document
+      .querySelector(".about-item-tech-group-list")
+      .style.setProperty("--tech-proficency-bar-width", "0%");
+    item
+      .querySelector(".tech-item-detail")
+      .classList.remove("tech-item-detail-show");
+    item.querySelector(".proficiency-bar").classList.remove("progressing");
+  })
+);
