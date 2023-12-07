ae498c250b423fc7185aac6d21f00d2f0fbcc065
qiyu6 <qiyu6@illinois.edu>
2023-12-07 00:04:27 -0600

    feat: refactor js code

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 // Part1: Nav Bar
-function handleNavTogglerClick() {
+document.addEventListener("DOMContentLoaded", () => {
+  const toggler = document.querySelector(".nav-bar-toggler");
+  const togglerLines = document.querySelectorAll(".nav-bar-toggler-line");
   const navBarList = document.querySelector(".nav-bar-list");
-  navBarList.classList.toggle("nav-bar-list-show");
 
-  const togglerLines = document.querySelectorAll(".toggler-line");
-  togglerLines[0].classList.toggle("toggler-cross-1");
-  togglerLines[1].classList.toggle("toggler-cross-2");
-  togglerLines[2].classList.toggle("toggler-cross-3");
-}
+  function handleTogglerClick() {
+    navBarList.classList.toggle("nav-bar-list-show");
+    togglerLines[0].classList.toggle("nav-bar-toggler-cross-1");
+    togglerLines[1].classList.toggle("nav-bar-toggler-cross-2");
+    togglerLines[2].classList.toggle("nav-bar-toggler-cross-3");
+  }
 
-function handleNavItemClick() {
-  const navBarList = document.querySelector(".nav-bar-list");
-  navBarList.classList.remove("nav-bar-list-show");
+  function handleNavItemClick(e) {
+    if (e.target.nodeName === "A") {
+      navBarList.classList.remove("nav-bar-list-show");
+      togglerLines[0].classList.remove("nav-bar-toggler-cross-1");
+      togglerLines[1].classList.remove("nav-bar-toggler-cross-2");
+      togglerLines[2].classList.remove("nav-bar-toggler-cross-3");
+    }
+  }
 
-  const togglerLines = document.querySelectorAll(".toggler-line");
-  togglerLines[0].classList.remove("toggler-cross-1");
-  togglerLines[1].classList.remove("toggler-cross-2");
-  togglerLines[2].classList.remove("toggler-cross-3");
-}
+  toggler.addEventListener("click", handleTogglerClick);
+  navBarList.addEventListener("click", handleNavItemClick);
+});
 
 // Part3: About
 const tech_proficiency_map = {
@@ -44,25 +49,11 @@ const tech_proficiency_map = {
   opencv: "70%",
 };
 
-const techList = document.querySelector(".about-item-tech-group-list");
-const techListItems = document.querySelectorAll(".about-tech-cat-list-item");
+document.addEventListener("DOMContentLoaded", () => {
+  const techList = document.querySelector(".about-item-tech-group-list");
+  const techListItems = document.querySelectorAll(".about-tech-cat-list-item");
 
-techListItems.forEach((techItem) =>
-  techItem.addEventListener("mouseenter", () => {
-    techList.style.setProperty(
-      "--tech-proficency-bar-width",
-      tech_proficiency_map[techItem.id]
-    );
-    techItem
-      .querySelector(".tech-item-detail")
-      .classList.add("tech-item-detail-show");
-    techItem.querySelector(".proficiency-bar").classList.add("progressing");
-  })
-);
-
-techList.addEventListener("click", (e) => {
-  if (e.target.classList.contains("about-tech-cat-list-item")) {
-    const techItem = e.target;
+  function showTechDetail(techItem) {
     techList.style.setProperty(
       "--tech-proficency-bar-width",
       tech_proficiency_map[techItem.id]
@@ -72,73 +63,81 @@ techList.addEventListener("click", (e) => {
       .classList.add("tech-item-detail-show");
     techItem.querySelector(".proficiency-bar").classList.add("progressing");
   }
-});
 
-techListItems.forEach((techItem) =>
-  techItem.addEventListener("mouseleave", () => {
+  function hideTechDetail(techItem) {
     techList.style.setProperty("--tech-proficency-bar-width", "0%");
     techItem
       .querySelector(".tech-item-detail")
       .classList.remove("tech-item-detail-show");
     techItem.querySelector(".proficiency-bar").classList.remove("progressing");
-  })
-);
-
-document.addEventListener("click", (e) => {
-  const flag = Array.from(techListItems).some((techItem) =>
-    techItem.contains(e.target)
-  );
-  if (!flag) {
-    document
-      .querySelector(".about-item-tech-group-list")
-      .style.setProperty("--tech-proficency-bar-width", "0%");
-    techListItems.forEach((techItem) => {
-      techItem
-        .querySelector(".tech-item-detail")
-        .classList.remove("tech-item-detail-show");
-      techItem
-        .querySelector(".proficiency-bar")
-        .classList.remove("progressing");
-    });
   }
+
+  techListItems.forEach((techItem) => {
+    techItem.addEventListener("mouseenter", () => showTechDetail(techItem));
+    techItem.addEventListener("mouseleave", () => hideTechDetail(techItem));
+  });
+
+  // compatable with mobile version
+  techList.addEventListener("click", (e) => {
+    if (e.target.classList.contains("about-tech-cat-list-item")) {
+      showTechDetail(e.target);
+    }
+  });
+
+  document.addEventListener("click", (e) => {
+    const flag = Array.from(techListItems).some((techItem) =>
+      techItem.contains(e.target)
+    );
+    if (!flag) {
+      techListItems.forEach((techItem) => {
+        hideTechDetail(techItem);
+      });
+    }
+  });
 });
 
 // Part5: Projects
-const toggleProjectsBtn = document.querySelector(".projects-collapsible-btn");
-const toggleProjectsBtnText = ["Show More", "Hide"];
-let toggleProjectsBtnIndex = 0;
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleProjectsBtn = document.querySelector(".projects-collapsible-btn");
+  const toggleProjectsBtnText = ["Show More", "Hide"];
+  let toggleProjectsBtnIndex = 0;
 
-function handleProjectListCollapse() {
-  const projectItems = Array.from(document.querySelectorAll(".projects-item"));
-  for (let i = 3; i < projectItems.length; i++) {
-    projectItems[i].classList.toggle("projects-item-hide");
-  }
-  toggleProjectsBtnIndex = 1 - toggleProjectsBtnIndex;
-  toggleProjectsBtn.innerText = toggleProjectsBtnText[toggleProjectsBtnIndex];
-}
+  toggleProjectsBtn.addEventListener("click", () => {
+    const projectItems = Array.from(
+      document.querySelectorAll(".projects-item")
+    );
+    for (let i = 3; i < projectItems.length; i++) {
+      projectItems[i].classList.toggle("projects-item-hide");
+    }
+    toggleProjectsBtnIndex = 1 - toggleProjectsBtnIndex;
+    toggleProjectsBtn.innerText = toggleProjectsBtnText[toggleProjectsBtnIndex];
+  });
+});
 
 // Part7: Contact
-const contactForm = document.querySelector("#contact-form");
-contactForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const name = contactForm.elements["name"].value;
-  const email = contactForm.elements["email"].value;
-  const message = contactForm.elements["message"].value;
+document.addEventListener("DOMContentLoaded", () => {
+  const contactForm = document.querySelector("#contact-form");
+  contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = contactForm.elements["name"].value;
+    const email = contactForm.elements["email"].value;
+    const message = contactForm.elements["message"].value;
 
-  const url = `https://docs.google.com/forms/d/e/1FAIpQLSch8gVBawLm_EHn1Za5EKUY1aaPpqjjvXbEUqufuDBplarD9g/formResponse?usp=pp_url&entry.1023284505=${name}&entry.1480291378=${email}&entry.364352625=${message}`;
-  fetch(url, { mode: "no-cors" })
-    .then((res) => {
-      console.log(res);
-      contactForm.reset();
-      alert(
-        "Your message has been received! Thank you. I will get back to you soon."
-      );
-    })
-    .catch((err) => {
-      console.log(err);
-      alert("Sorry, something went wrong! Please try again.");
-    })
-    .finally(() => {
-      contactForm.reset();
-    });
+    const url = `https://docs.google.com/forms/d/e/1FAIpQLSch8gVBawLm_EHn1Za5EKUY1aaPpqjjvXbEUqufuDBplarD9g/formResponse?usp=pp_url&entry.1023284505=${name}&entry.1480291378=${email}&entry.364352625=${message}`;
+    fetch(url, { mode: "no-cors" })
+      .then((res) => {
+        console.log(res);
+        contactForm.reset();
+        alert(
+          "Your message has been received! Thank you. I will get back to you soon."
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Sorry, something went wrong! Please try again.");
+      })
+      .finally(() => {
+        contactForm.reset();
+      });
+  });
 });
